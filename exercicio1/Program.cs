3007840707f70e8338df9c5a9619522e827fe210
Seu nome para exibiçãoMartarelli <renanmartarelli@gmail.com>
2022-08-19 04:41:27 -0300

    primeira aula de funcoes recursivas

---
+++ exercicio1/Program.cs
@@ -0,0 +1,52 @@
+﻿string op = "0";
+while (op != "3")
+{
+    Console.Clear();
+    Console.WriteLine("MENU PRINCIPAL");
+    Console.WriteLine("1 - Função sem vetor");
+    Console.WriteLine("2 - Função com vetor");
+    Console.WriteLine("3 - Sair");
+    Console.Write("Digite a opção desejada: ");
+    op = Console.ReadLine();
+
+    if (op == "1")
+    {
+        int nI, nF;
+        Console.Write("Numero Inicial: ");
+        nI = int.Parse(Console.ReadLine());
+        Console.Write("Numero Final: ");
+        nF = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("MENU 2");
+        Console.WriteLine("1 - Crescente");
+        Console.WriteLine("2 - Decrescente");
+        Console.WriteLine("3 - Impares");
+        Console.WriteLine("4 - Somatório");
+        string op2 = Console.ReadLine();
+        if (op2 == "1")
+        {
+            Console.WriteLine("De " + nI + " até " + nF + " em ordem crescente:");
+            crescente(nI,nF);
+        } else if (op2 == "2") {
+            Console.WriteLine("De " + nI + " até " + nF + " em ordem decrescente:");
+            decrescente(nI,nF);
+        }
+    }
+    Console.ReadKey();
+}
+
+void crescente(int nI, int nF){
+    if (nI <= nF)
+    {
+        Console.WriteLine(nI);
+        crescente(nI+1,nF);
+    }
+}
+
+void decrescente(int nI, int nF){
+    if (nF <= nI)
+    {
+        Console.WriteLine(nF);
+        crescente(nI,nF-1);
+    }
+}
