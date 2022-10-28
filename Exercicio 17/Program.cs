87a2be0dcc44123394d100d8f2dd29d0cac202de
Martarelli <renanmartarelli@gmail.com>
2022-10-27 21:42:50 -0300

    exercicio finalizado

--- Exercicio 17/Program.cs
+++ Exercicio 17/Program.cs
@@ -1,5 +1,39 @@
 ﻿// 17) Faça uma implementação que construa uma lista encadeada. Seu programa deve ter as opções de inserção e remoção dos elementos. Após remover um elemento da lista, exiba-o na tela.
 
+string op = "1";
+int x;
+tp_no lista = null;
+tp_no elemento = null;
+while (op != "0")
+{
+    Console.WriteLine("1 - Adicionar na lista");
+    Console.WriteLine("2 - Remover da lista"); 
+    Console.WriteLine("0 - Sair"); 
+    Console.Write("Escolha uma opção: ");
+
+    op = Console.ReadLine();
+
+    if (op == "1")
+    {
+        Console.Write("Digite o número que deseja adicionar: ");
+        x = Convert.ToInt32(Console.ReadLine());
+        Insere(ref lista, x);
+    }
+    else if (op == "2")
+    {
+        elemento = Remove(ref lista);
+        if (elemento != null)
+        {
+            Console.WriteLine("Valor removido: " + elemento.valor);
+        }
+        else
+        {
+            Console.WriteLine("Lista vazia");
+        }
+    }
+
+}
+
 void Insere(ref tp_no l, int v)
 {
     tp_no no = new tp_no();
@@ -21,11 +55,9 @@ tp_no Remove(ref tp_no l)
     return no;
 }
 
-tp_no lista = null;
 
 class tp_no
 {
     public int valor;
     public tp_no prox;
 }
-
