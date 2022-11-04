d46f1c6df16f5727687ff07d4267bc5da0f6ffe4
Martarelli <renanmartarelli@gmail.com>
2022-11-03 22:00:11 -0300

    adicionado inserir e sair

--- Exercicio 18/Program.cs
+++ Exercicio 18/Program.cs
@@ -1,7 +1,64 @@
-﻿void Insere(ref tp_no l, int v)
+﻿tp_no lista = null;
+string op = "1";
+string op2 = "";
+string nome, idade, whats = "";
+
+while (op != "0")
+{
+    Console.WriteLine("MENU PRINCIPAL");
+    Console.WriteLine("1 - Inserir Cadastro");
+    Console.WriteLine("2 - Alterar Cadastro");
+    Console.WriteLine("3 - Excluir Cadastro");
+    Console.WriteLine("4 - Exibir Cadastro");
+    Console.WriteLine("0 - Sair");
+
+    Console.Write("Digite a opção desejada: ");
+    op2 = Console.ReadLine();
+
+    if (op2 == "1")
+    {
+        Console.Write("Digite o nome: ");
+        nome = Console.ReadLine();
+        Console.Write("Digite a idade: ");
+        idade = Console.ReadLine();
+        Console.Write("Digite o zapzap: ");
+        whats = Console.ReadLine();
+
+        Insere(ref lista, nome, idade, whats);
+
+    }
+    else if (op2 == "2")
+    {
+
+    }
+    else if (op2 == "3")
+    {
+
+    }
+    else if (op2 == "4")
+    {
+
+    }
+    else if (op2 == "0")
+    {
+        Console.WriteLine("Encerrando a aplicação....");
+        op = "0";
+    }
+    else
+    {
+        Console.WriteLine("Opção inválida... Tente novamente...");
+    }
+
+
+}
+
+
+void Insere(ref tp_no l, string n, string i, string w)
 {
     tp_no no = new tp_no();
-    no.valor = v;
+    no.nome = n;
+    no.idade = i;
+    no.whats = w;
     if (l != null)
         no.prox = l;
     l = no;
@@ -19,10 +76,10 @@ tp_no Remove(ref tp_no l)
     return no;
 }
 
-
 class tp_no
 {
-    public int valor;
+    public string nome, idade, whats;
     public tp_no prox;
 }
+
 
