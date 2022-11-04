2c8b053309d213e69818b326d3a55114fd11ce85
Martarelli <renanmartarelli@gmail.com>
2022-11-03 21:16:36 -0300

    criado projeto exercicio 18

---
+++ Exercicio 18/Program.cs
@@ -0,0 +1,28 @@
+﻿void Insere(ref tp_no l, int v)
+{
+    tp_no no = new tp_no();
+    no.valor = v;
+    if (l != null)
+        no.prox = l;
+    l = no;
+}
+
+tp_no Remove(ref tp_no l)
+{
+    tp_no no = null;
+    if (l != null)
+    {
+        no = l;
+        l = l.prox;
+        no.prox = null;
+    }
+    return no;
+}
+
+
+class tp_no
+{
+    public int valor;
+    public tp_no prox;
+}
+
