39a788a9fe5f8dc47206658817e139e84ccd81a9
Seu nome para exibiçãoMartarelli <renanmartarelli@gmail.com>
2022-11-26 17:00:15 -0300

    instruções do exercicio

--- Exercicio 28/Program.cs
+++ Exercicio 28/Program.cs
@@ -1,2 +1,6 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// 28) Implemente um programa com as seguintes opções: Sem tratamento de colisão, Tratamento de colisão Linear e Tratamento de colisão com Lista Encadeada.
+// 	Dentro de cada opção deve haver as funcionalidades: Inserir, Alterar e Relatar.
+// 	O vetor deve ser do tipo abstrato de dado composto por idade, nome e whats. Serão necessários 3 vetores, um para cada tipo de tratamento de colisão.
+// Para inserir um novo registro, solicite a idade, nome e whats. Utilize a idade como chave.
+// Para alterar solicite a idade (chave) para ser utilizada na busca. Caso encontrada, informe o nome e o whats da pessoa. Após a consulta, o usuário pode atualizar somente o nome e o whats.
+// Para relatar, percorra o vetor do inicio ao fim e exiba todos os registros.
